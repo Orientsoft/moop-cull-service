1d34103940e5cbe3cd293fa9ad9e6a3f6366a5bb
xiedidan <xiedidan@yeah.net>
2019-05-07 14:31:14 +0800

    minor fix

--- cull_service.py
+++ cull_service.py
@@ -257,14 +257,13 @@ def cull_idle(
             delete_url = url + '/users/%s/server' % quote(user['name'])
 
         # TODO : call es_service to record stopping event
-        body = json.dump({
+        body = json.dumps({
             'user_name': user['name'],
             'end': now,
             'last_activity': server['last_activity'],
             'tenant_id': tenant
         })
         print(body)
-        '''
         req = HTTPRequest(
             url='{}/notify/end'.format(es_service_url),
             method='POST',
@@ -272,8 +271,7 @@ def cull_idle(
         )
         resp = yield fetch(req)
         logger.debug('es_service req: {}\nresp: {}'.format(body, resp.code))
-        '''
-        
+
         req = HTTPRequest(url=delete_url, method='DELETE', headers=auth_header)
         resp = yield fetch(req)
         if resp.code == 202:
